4d2909248b308720fa8037c01ec2a90911238f0a
James Pacheco <jamesrp13@gmail.com>
2022-08-30 10:26:34 -0600

    feat: add landing and mint pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app"
+import { ChakraProvider } from "@chakra-ui/react"
+
+import { extendTheme } from "@chakra-ui/react"
+import WalletContextProvider from "../components/WalletContextProvider"
+
+const colors = {
+  background: "#1F1F1F",
+  accent: "#833BBE",
+  bodyText: "rgba(255, 255, 255, 0.75)",
+}
+
+const theme = extendTheme({ colors })
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <WalletContextProvider>
+        <Component {...pageProps} />
+      </WalletContextProvider>
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
